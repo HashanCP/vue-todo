f55b5c172d68a6d3db116376395ae772013b1fca
Hashan Chandika <hashan@arimaclanka.com>
2019-03-05 15:46:16 +0530

    completed todo app

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ new Vue({
   el: "#app",
 
   data: {
-    message: "Hello HTML!"
+    todos: [
+      { text: "Learn JavaScript", done: false },
+      { text: "Learn Vue", done: false },
+      { text: "Learn Electron", done: false },
+      { text: "Build Apps", done: false }
+    ],
+    newTodoItem: ''
   },
 
   methods: {
-    changeTitle() {
-      this.message = "Greetings from Vue.js"
+    toggle(todoItem) {
+      todoItem.done = !todoItem.done
+    },
+
+    addNewTodoItem() {
+      this.todos.push({
+        text: this.newTodoItem,
+        done: false
+      });
+      this.newTodoItem = ''
     }
   }
 });
